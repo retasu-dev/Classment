22e697e7b1997caaea994ed4c7dc743c431f7a44
retasu-dev <retasu378@gmail.com>
2025-12-15 22:27:24 +0900

    🔧 fix: remove unnecessary ESLint disable comments in configuration files

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { doPost } from './event/line/doPost';
 import { PostBroadcast } from './libs/line/webhook/broadcast';
 import { broadcastWeekEvents } from './trigger/calendar/broadcastWeekEvents';
 import { broadcastAnnounce } from './trigger/announce/broadcastAnnounce';
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare let global: any;
 
 global.broadcastTest = () => {
